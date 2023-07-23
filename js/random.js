22dce6c940c2d3b6dc3a1bdf58cf33e9720084c8
developerSumon52 <140273215+developerSumon52@users.noreply.github.com>
2023-07-23 20:16:20 +0000

    many

---
+++ js/random.js
@@ -0,0 +1,8 @@
+console.log(Math.random());
+console.log(Math.random() * 10 + 1);
+
+
+// ++++++ Output a random number between two number and the output will be including two numbers
+const max = 20
+const min = 18
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
