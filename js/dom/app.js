92e13203761c7530b59c690bcad1a32f23a1be02
developerSumon52 <140273215+developerSumon52@users.noreply.github.com>
2023-07-29 18:36:19 +0000

    ff

--- js/dom/app.js
+++ js/dom/app.js
@@ -1,3 +1,8 @@
 
 let heading = document.getElementById('heading')
 heading.innerHTML= "dom"
+ let mylist = document.querySelectorAll('li')
+ mylist.forEach((li)=>{
+    li.style.backgroundColor = "red"
+ })
+ console.log(mylist);
